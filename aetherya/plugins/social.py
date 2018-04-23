ca4b72040c396f5b2dc432c1b27f48bc899076e8
OGNova <jamooney2000@gmail.com>
2018-04-22 23:20:43 -0400

    Make stuff work.

--- aetherya/plugins/social.py
+++ aetherya/plugins/social.py
@@ -4,10 +4,13 @@ import json
 
 from disco.bot import Plugin
 
+from os import listdir
+
 DATA_DIR = 'data/guilds/settings/{}.json'
 DEFAULT_CONFIG = 'data/default_config.json'
 
 POINTS_DIR = 'data/guilds/points/{}.json'
+DEFAULT_POINTS = 'data/default_points.json'
 
 # TODO: Make this shit actually work.
 class SocialPlugin(Plugin):
@@ -19,21 +22,23 @@ class SocialPlugin(Plugin):
 
   @Plugin.listen('MessageCreate')
   def message_listener(self, event):
+    if event.message.author.id == 379402095914123264:
+      return
+
     with open(DATA_DIR.format(event.message.guild.id), 'r') as file:
       data = json.load(file)
 
-    if event.message.content.startswith(data['prefix']):
-      return
-    
-    min = int(data['minPoints'])
-    max = int(data['maxPoints'])
+      min = int(data['minPoints'])
+      max = int(data['maxPoints'])
 
-    points = self.giveRandomPoints(min, max)
-    print(points)
+      if event.message.content.startswith(data['prefix']):
+        return
 
-    with open(POINTS_DIR.format(event.message.author.id), 'w') as file:
-      data = json.load(file)
+      with open(POINTS_DIR.format(event.message.author.id), 'r') as file:
+        data = json.load(file)
+
+      points = self.giveRandomPoints(min, max)
 
       data['points'] = int(data['points']) + points
-
-      file.write(json.dumps(data, indent=2))
+      with open(POINTS_DIR.format(event.message.author.id), "w") as file:
+        file.write(json.dumps(data, indent=2))
